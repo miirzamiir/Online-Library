213ed4c4c6d87a0dffd6b9b72db34d4b6772e37f
miirzamiir <miirzamiir@gmail.com>
2022-03-31 20:44:58 +0430

    creating user management endpoint

--- user/serializers.py
+++ user/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import User
 
+
 class CreateUserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
@@ -16,3 +17,30 @@ class CreateUserSerializer(serializers.ModelSerializer):
                 'style':{'input_type':'password',}
             }
         }
+
+
+class ManageUserSerializer(serializers.ModelSerializer):
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password',None)
+        user = super().update(**validated_data)
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
+
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'name', 'password')
+        read_only_fields = ('id',)
+        write_only_fields = ('password',)
+        extra_kwargs = {
+            'password':{
+                'write_only':True,
+                'style':{'input_type':'password',}
+            }
+        }
+
--- user/views.py
+++ user/views.py
@@ -1,7 +1,21 @@
-from rest_framework.generics import CreateAPIView
+from django.shortcuts import get_object_or_404
+from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import User
-from .serializers import CreateUserSerializer
+from .serializers import CreateUserSerializer, ManageUserSerializer
+
+
+class TokenAPI():
+    pass
+
 
 class CreateUserAPI(CreateAPIView):
     queryset = User.objects.all()
     serializer_class = CreateUserSerializer
+
+
+class ManageUserAPI(RetrieveUpdateDestroyAPIView):
+    
+    serializer_class = ManageUserSerializer
+
+    def get_object(self):
+        return self.request.user
