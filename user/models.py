f34d39c8b101fd09870bc0c26c729529bba953eb
miirzamiir <miirzamiir@gmail.com>
2022-04-10 00:00:58 +0430

    superuser creating bugs fixed

--- user/models.py
+++ user/models.py
@@ -41,7 +41,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'username'
-    REQUIERED_FIELDS = ('email', 'password')
+    REQUIRED_FIELDS = ('email', 'password')
 
     def __str__(self):
         return self.username
